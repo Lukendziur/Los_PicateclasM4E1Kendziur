2686e290f06832e28e1399cd2eb943baf6970c7d
LuKendziur <lukendziur@hotmail.com>
2021-03-26 00:54:14 -0300

    subiendo register y login

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 3030
 const path = require('path');
 
 app.use(express.static('public'));
-
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'));
-});
 
 app.listen(port, () => {
     console.log('Example app listening on port '+ port);
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
